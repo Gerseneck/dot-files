d715dda264876c8d88338eb44fb2e45abcf0f300
Geeson Wan <wang14240@gmail.com>
2023-02-13 20:03:35 -0500

    greatstudier gamify

---
+++ greatstudier_gamify.py
@@ -0,0 +1,4 @@
+# Data for GreatStudier
+# Please do not touch this!
+
+{'level': 19, 'xp': 1378, 'correct_answers': 3128, 'wrong_answers': 137, 'quests': {'login_bonus': {'last_reset': '2023-02-13', 'completed': True, 'progress': 1}, 'study_50': {'last_reset': '2023-02-13', 'completed': True, 'progress': 50}, 'answer_correct_500': {'last_reset': '2022-12-19', 'completed': False, 'progress': 417}, 'review_100': {'last_reset': '2000-01-01', 'completed': False, 'progress': 0}, 'study_hours': {'last_reset': '2022-12-19', 'completed': False, 'progress': 294728}}, 'rev': 1, 'total_time_studied': 1498881}
