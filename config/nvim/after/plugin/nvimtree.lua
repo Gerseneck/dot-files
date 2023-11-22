f8bee1a3b8da93c6e3e992b41dd44d201adc8259
Gerseneck <wang14240@gmail.com>
2023-11-22 15:10:45 -0500

    updated nvimtree icons

--- config/nvim/after/plugin/nvimtree.lua
+++ config/nvim/after/plugin/nvimtree.lua
@@ -1,5 +1,25 @@
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+   renderer = {
+       icons = {
+           show = {
+               folder_arrow = false;
+           },
+           glyphs = {
+               git = {
+                   unstaged = '[M]',
+                   staged = '[+]',
+                   unmerged = '',
+                   renamed = '[R]',
+                   deleted = '[✘]',
+                   untracked = '[?]',
+                   ignored = ''
+               }
+
+           }
+       }
+   }
+})
 
 vim.keymap.set({'n', 'v', 'i'}, '<C-f>', '<cmd>NvimTreeToggle<cr>',
-	{silent = true, noremap = true}
+	{ silent = true, noremap = true }
 )
