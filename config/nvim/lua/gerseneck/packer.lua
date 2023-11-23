61e4a9d34ba8b10944494bd8828895dc48923b5f
Gerseneck <wang14240@gmail.com>
2023-11-23 13:27:51 -0500

    some more nvim plugins

--- config/nvim/lua/gerseneck/packer.lua
+++ config/nvim/lua/gerseneck/packer.lua
@@ -3,7 +3,7 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
 
     use 'wbthomason/packer.nvim'
-    use 'nvim-tree/nvim-tree.lua' 
+    use 'nvim-tree/nvim-tree.lua'
     use 'lifepillar/pgsql.vim'
     use 'nvim-tree/nvim-web-devicons'
     use 'tamton-aquib/staline.nvim'
@@ -13,10 +13,16 @@ return require('packer').startup(function(use)
     use 'terryma/vim-multiple-cursors'
     use 'vimlab/split-term.vim'
     use 'lewis6991/gitsigns.nvim'
+    use { 'kylechui/nvim-surround', tag = '*' }
 
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.2',
         requires = { {'nvim-lua/plenary.nvim'} }
+    }
+
+    use {
+        'windwp/nvim-autopairs',
+        config = function() require('nvim-autopairs').setup {} end
     }
 
     use {
