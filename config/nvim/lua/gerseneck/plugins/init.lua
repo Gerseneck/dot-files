a3669e9ae60519b249423c0320356b84398b5935
Gerseneck <wang14240@gmail.com>
2024-02-21 23:28:53 -0500

    updated plugins

--- config/nvim/lua/gerseneck/plugins/init.lua
+++ config/nvim/lua/gerseneck/plugins/init.lua
@@ -2,7 +2,6 @@ return {
   "nvim-tree/nvim-web-devicons",
   "tamton-aquib/staline.nvim",
   "ap/vim-css-color",
-  "ryanoasis/vim-devicons",
   "nvim-treesitter/nvim-treesitter",
   "lervag/vimtex",
   { "kylechui/nvim-surround", version = "*" }
