260260b96f81b88719fbf6500fc837231c1947de
Gerseneck <wang14240@gmail.com>
2024-03-13 22:35:09 -0400

    updated telescope config

--- config/nvim/lua/gerseneck/plugins/telescope.lua
+++ config/nvim/lua/gerseneck/plugins/telescope.lua
@@ -2,14 +2,6 @@ return {
   "nvim-telescope/telescope.nvim", branch = "0.1.x",
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
-    require("telescope").setup({
-      pickers = {
-        find_files = {
-          hidden = true
-        }
-      }
-    })
-
     local builtin = require("telescope.builtin")
     vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
     vim.keymap.set("n", "<leader>fp", builtin.git_files, {})
