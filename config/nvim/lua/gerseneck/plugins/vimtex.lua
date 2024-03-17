e23aecc902920e064bc84581498a25a84f2e38a2
Gerseneck <wang14240@gmail.com>
2024-03-16 20:35:04 -0400

    vimtex: automatically clean junk files

--- config/nvim/lua/gerseneck/plugins/vimtex.lua
+++ config/nvim/lua/gerseneck/plugins/vimtex.lua
@@ -2,5 +2,15 @@ return {
   "lervag/vimtex",
   init = function()
     vim.g.tex_flavor = "latex"
+
+    local vimtex_autogroup = vim.api.nvim_create_augroup("vimtex", { clear = true })
+
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "VimtexEventCompileStopped",
+      group = vimtex_autogroup,
+      callback = function()
+        vim.cmd("silent! VimtexClean")
+      end
+    })
   end
 }
