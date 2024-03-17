7ac1d5286949452313d139c51245dcd9ac3f9e0c
Gerseneck <wang14240@gmail.com>
2024-03-17 16:30:33 -0400

    2 space indentation for lua

--- config/nvim/lua/gerseneck/core/autocmd.lua
+++ config/nvim/lua/gerseneck/core/autocmd.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "html", "htm" },
+  pattern = { "html", "lua" },
   callback = function()
     vim.opt_local.shiftwidth = 2
     vim.opt_local.tabstop = 2
