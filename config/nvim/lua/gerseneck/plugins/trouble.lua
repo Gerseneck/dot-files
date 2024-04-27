46ce12336344b0a75f75bd3415c05d416788be19
Gerseneck <wang14240@gmail.com>
2024-04-27 10:29:50 -0400

    added trouble.nvim

---
+++ config/nvim/lua/gerseneck/plugins/trouble.lua
@@ -0,0 +1,20 @@
+return {
+  "folke/trouble.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    local trouble = require("trouble")
+    trouble.setup({})
+
+    vim.keymap.set("n", "<leader>tt", function()
+      trouble.toggle()
+    end)
+
+    vim.keymap.set("n", "[t", function()
+      trouble.next({skip_groups = true, jump = true});
+    end)
+
+    vim.keymap.set("n", "]t", function()
+      trouble.previous({skip_groups = true, jump = true});
+    end)
+  end
+}
