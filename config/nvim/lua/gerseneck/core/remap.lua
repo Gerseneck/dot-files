99796733b12f6b680b9ab836512ae2a055f85fe7
Gerseneck <wang14240@gmail.com>
2024-04-27 10:19:13 -0400

    copy to system clipboard remap

--- config/nvim/lua/gerseneck/core/remap.lua
+++ config/nvim/lua/gerseneck/core/remap.lua
@@ -2,3 +2,7 @@ vim.g.mapleader = " "
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- yank to system clipboard
+vim.keymap.set("n", "<leader>y", [[ "+Y ]])
+vim.keymap.set("v", "<leader>y", [[ "+y ]])
