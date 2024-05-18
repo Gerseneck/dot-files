f1edf92ddeb459271322ef6b69869e695c0224d3
Gerseneck <wang14240@gmail.com>
2024-05-18 14:47:49 -0400

    ruby-lsp fix

--- config/nvim/lua/gerseneck/plugins/lsp/lsp_config.lua
+++ config/nvim/lua/gerseneck/plugins/lsp/lsp_config.lua
@@ -12,6 +12,34 @@ return {
     local lsp_config = require("lspconfig")
     local capabilities = vim.lsp.protocol.make_client_capabilities()
 
+    local function add_ruby_deps_command(client, bufnr)
+      vim.api.nvim_buf_create_user_command(bufnr, "ShowRubyDeps", function(opts)
+        local params = vim.lsp.util.make_text_document_params()
+        local showAll = opts.args == "all"
+
+        client.request("rubyLsp/workspace/dependencies", params, function(error, result)
+          if error then
+            print("Error showing deps: " .. error)
+            return
+          end
+
+          local qf_list = {}
+          for _, item in ipairs(result) do
+            if showAll or item.dependency then
+              table.insert(qf_list, {
+                text = string.format("%s (%s) - %s", item.name, item.version, item.dependency),
+                filename = item.path
+              })
+            end
+          end
+
+          vim.fn.setqflist(qf_list)
+          vim.cmd('copen')
+        end, bufnr)
+      end,
+      {nargs = "?", complete = function() return {"all"} end})
+    end
+
     require("mason").setup({})
     require("mason-lspconfig").setup({
       ensure_installed = {
@@ -31,7 +59,6 @@ return {
             capabilities = capabilities
           })
         end,
-
         ["lua_ls"] = function()
           lsp_config.lua_ls.setup({
             capabilities = capabilities,
@@ -42,6 +69,14 @@ return {
                 }
               }
             }
+          })
+        end,
+        ["ruby_lsp"] = function()
+          lsp_config.ruby_lsp.setup({
+            capabilities = capabilities,
+            on_attach = function(client, buffer)
+              add_ruby_deps_command(client, buffer)
+            end
           })
         end
       }
