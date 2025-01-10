029bc8a7b958f44b01b95504d389d90a58629f73
Gerseneck <wang14240@gmail.com>
2025-01-09 21:30:38 -0500

    feat: neogen nvim plugin configurations

---
+++ config/nvim/lua/gerseneck/plugins/neogen.lua
@@ -0,0 +1,18 @@
+return {
+  "danymat/neogen",
+  version = "*",
+  config = function()
+    local neogen = require("neogen")
+    neogen.setup({
+      enabled = true,
+      languages = {
+        java = { template = { annotation_convention = "javadoc" }},
+        lua = { template = { annotation_convention = "emmylua" }},
+        python = { template = { annotation_convention = "google_docstrings" }},
+        ruby = { template = { annotation_convention = "rdoc" }}
+      }
+    })
+
+    vim.keymap.set("n", "<leader>ng", neogen.generate)
+  end
+}
