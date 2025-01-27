bb31990b89936ac58eba0779c710bcfdfeb84179
Gerseneck <wang14240@gmail.com>
2025-01-27 16:16:24 -0500

    feat: some more lsp related keybindings

--- config/nvim/lua/gerseneck/core/autocmd.lua
+++ config/nvim/lua/gerseneck/core/autocmd.lua
@@ -14,6 +14,8 @@ vim.api.nvim_create_autocmd("LspAttach", {
     vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, opts)
     vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
     vim.keymap.set("n", "<leader>gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>cr", function() vim.lsp.buf.references() end, opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
   end
 })
