900b4635520e7072b4488eac5cf7250b03114e0f
Gerseneck <wang14240@gmail.com>
2025-02-14 11:41:40 -0500

    feat: latexmk compiler config for vimtex

--- config/nvim/lua/gerseneck/plugins/vimtex.lua
+++ config/nvim/lua/gerseneck/plugins/vimtex.lua
@@ -3,6 +3,10 @@ return {
   ft = { "tex" },
   init = function()
     vim.g.tex_flavor = "latex"
+    vim.g.vimtex_compiler_method = "latexmk"
+    vim.g.vimtex_compiler_latexmk = {
+      aux_dir = vim.fn.expand("~/.cache/latexmk/aux_files/")
+    }
 
     local vimtex_autogroup = vim.api.nvim_create_augroup("vimtex", { clear = true })
 
