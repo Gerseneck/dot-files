a52645dcd4a929eab44b369b5165634d1aa1180f
Gerseneck <wang14240@gmail.com>
2025-03-20 15:43:01 -0400

    feat(nvim): ccc for color picking and highlighting

---
+++ config/nvim/lua/gerseneck/plugins/ccc.lua
@@ -0,0 +1,22 @@
+return {
+  "uga-rosa/ccc.nvim",
+  config = function()
+    local ccc = require("ccc")
+
+    ccc.setup({
+      lsp = true,
+      highlighter = {
+        auto_enable = true,
+        lsp = true,
+        filetypes = { "html", "css", "scss", "sass" }
+      },
+      recognize = {
+        input = true,
+        output = true
+      }
+    })
+
+    vim.keymap.set("n", "<leader>cc", "<cmd>CccPick<cr>")
+    vim.keymap.set("n", "<leader>cf", "<cmd>CccConvert<cr>")
+  end
+}
--- config/nvim/lua/gerseneck/plugins/init.lua
+++ config/nvim/lua/gerseneck/plugins/init.lua
@@ -1,4 +1,3 @@
 return {
-  "nvim-tree/nvim-web-devicons",
-  "ap/vim-css-color"
+  "nvim-tree/nvim-web-devicons"
 }
