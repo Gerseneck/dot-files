6c1dc6b7ec73e43c35184671b0f2bbc88d52a1c0
Gerseneck <wang14240@gmail.com>
2025-06-14 14:36:21 -0400

    feat(nvim): more keymaps

--- config/nvim/lua/gerseneck/core/remap.lua
+++ config/nvim/lua/gerseneck/core/remap.lua
@@ -1,8 +1,10 @@
 vim.g.mapleader = " "
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv")
 
 -- yank to system clipboard
 vim.keymap.set("n", "<leader>y", [[ "+Y ]])
 vim.keymap.set("v", "<leader>y", [[ "+y ]])
+
+vim.keymap.set("n", "gG", "ggVG")
