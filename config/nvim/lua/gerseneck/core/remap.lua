49b7e5dc68db95deef0ef749e2be40b1e4c7d875
Gerseneck <wang14240@gmail.com>
2025-07-18 15:05:26 -0400

    feat(nvim): keymap for yanking current filename (relative) and line number to `"+` register

--- config/nvim/lua/gerseneck/core/remap.lua
+++ config/nvim/lua/gerseneck/core/remap.lua
@@ -1,5 +1,6 @@
 vim.g.mapleader = " "
 
+-- move blocks of code up and down
 vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv")
 
@@ -7,4 +8,14 @@ vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "<leader>y", [[ "+Y ]])
 vim.keymap.set("v", "<leader>y", [[ "+y ]])
 
+-- select entire file content
 vim.keymap.set("n", "gG", "ggVG")
+
+-- yank filename (relative path) and line number to clipboard
+vim.keymap.set("n", "<leader>cl", function()
+  local filename_line_number = vim.fn.expand("%") .. ":" .. vim.fn.getcurpos()[2]
+
+  vim.fn.setreg("+", filename_line_number)
+
+  print(string.format("Yanked current filename (relative) and line number to @+: %s", filename_line_number))
+end)
