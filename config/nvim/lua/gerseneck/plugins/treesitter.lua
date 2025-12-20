34e829973d0a6f089dcd11ebb4402b0469a46cd4
Gerseneck <wang14240@gmail.com>
2025-12-19 23:37:51 -0500

    feat(nvim): treesitter `main` branch config update

--- config/nvim/lua/gerseneck/plugins/treesitter.lua
+++ config/nvim/lua/gerseneck/plugins/treesitter.lua
@@ -1,26 +1,43 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  branch = "master",
+  branch = "main",
   build = ":TSUpdate",
   config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "vimdoc",
-        "python",
-        "java",
-        "ruby",
-        "lua",
-        "typescript",
-        "javascript",
-      },
-      indent = {
-        enable = false,
-      },
-      highlight = {
-        enable = true,
-        disable = { "html" },
-        additional_vim_regex_highlighting = { "markdown" },
-      },
+    local treesitter = require("nvim-treesitter")
+
+    local parsers = {
+      "vimdoc",
+      "python",
+      "java",
+      "ruby",
+      "lua",
+      "typescript",
+      "javascript",
+      "markdown",
+    }
+
+    treesitter.install(parsers)
+
+    -- filetypes might have different names than parsers
+    local filetypes = {
+      "help",
+      "python",
+      "java",
+      "ruby",
+      "lua",
+      "typescript",
+      "javascript",
+      "markdown",
+    }
+
+    local treesitter_autogroup = vim.api.nvim_create_augroup("treesitter", { clear = true })
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = filetypes,
+      group = treesitter_autogroup,
+      callback = function()
+        vim.treesitter.start()
+      end,
     })
   end,
 }
